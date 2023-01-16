11eb67badae854fa008e23866d8683f20473a42c
Dmitry Ushakov <dimau95@yandex.ru>
2023-01-16 15:00:37 +0600

    commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Diary from './Diary';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Diary />
+    </>
+  );
+}
+
+export default App;
